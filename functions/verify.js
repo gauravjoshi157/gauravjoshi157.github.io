38835043af8ec490a63822a531b6ce5d9fc048c8
Gaurav joshi <gaurav157joshi@gmail.com>
2025-11-18 16:27:35 +0530

    Add onRequestPost function for token verification

---
+++ functions/verify.js
@@ -0,0 +1,21 @@
+export async function onRequestPost(context) {
+    const { token } = await context.request.json();
+    
+    const secret = "0x4AAAAAACBjgDzX2JiTFHtPs0fri2Y5aU8
+";
+
+    const formData = new FormData();
+    formData.append("secret", secret);
+    formData.append("response", token);
+
+    const result = await fetch(
+        "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+        { method: "POST", body: formData }
+    );
+
+    const outcome = await result.json();
+
+    return new Response(JSON.stringify({ success: outcome.success }), {
+        headers: { "Content-Type": "application/json" }
+    });
+}
